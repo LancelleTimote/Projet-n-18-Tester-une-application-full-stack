5b2f63e4d642e174d10e89c306921eca1b9a38a3
Timoté LANCELLE <timote.lancelle@outlook.fr>
2024-10-11 11:18:43 +0200

    Test Jest session.service

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -1,17 +1,48 @@
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 import { SessionService } from './session.service';
 
 describe('SessionService', () => {
   let service: SessionService;
+  let mockSessionInfo: SessionInformation;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SessionService);
+    mockSessionInfo = {
+      token: 'testToken',
+      type: 'Bearer',
+      id: 1,
+      username: 'testUser',
+      firstName: 'John',
+      lastName: 'Doe',
+      admin: false,
+    };
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should log in the user', () => {
+    service.logIn(mockSessionInfo);
+    expect(service.isLogged).toBeTruthy();
+    expect(service.sessionInformation).toEqual(mockSessionInfo);
+
+    service.$isLogged().subscribe((isLogged) => {
+      expect(isLogged).toBeTruthy();
+    });
+  });
+
+  it('should log out the user', () => {
+    service.logOut();
+    expect(service.isLogged).toBeFalsy();
+    expect(service.sessionInformation).toBeUndefined();
+
+    service.$isLogged().subscribe((isLogged) => {
+      expect(isLogged).toBeFalsy();
+    });
   });
 });
