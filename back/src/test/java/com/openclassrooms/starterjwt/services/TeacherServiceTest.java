650d4f6d6f8ad428776643c1e9b7516b3a03a78a
Timoté LANCELLE <timote.lancelle@outlook.fr>
2024-11-10 13:02:05 +0100

    Passage des tests unitaires du teacherService en tests d'intégrations

--- back/src/test/java/com/openclassrooms/starterjwt/services/TeacherServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/TeacherServiceTest.java
@@ -4,51 +4,58 @@ import com.openclassrooms.starterjwt.models.Teacher;
 import com.openclassrooms.starterjwt.repository.TeacherRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
+@SpringBootTest
+@Transactional
 class TeacherServiceTest {
 
-    @Mock
-    private TeacherRepository teacherRepository;
-
-    @InjectMocks
+    @Autowired
     private TeacherService teacherService;
+
+    @Autowired
+    private TeacherRepository teacherRepository;
 
     private Teacher teacher;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
-        teacher = new Teacher().setId(1L).setFirstName("Margot").setLastName("DELAHAYE");
+        teacher = new Teacher().setFirstName("Margot").setLastName("DELAHAYE");
     }
 
     @Test
     void findAll_ShouldReturnListOfTeachers() {
-        when(teacherRepository.findAll()).thenReturn(List.of(teacher));
+        teacherRepository.save(teacher);
+
         List<Teacher> teachers = teacherService.findAll();
-        assertEquals(1, teachers.size());
-        assertEquals(teacher, teachers.get(0));
+
+        assertEquals(3, teachers.size());
+
+        assertTrue(teachers.stream().anyMatch(t -> t.getFirstName().equals(teacher.getFirstName()) &&
+                t.getLastName().equals(teacher.getLastName())));
     }
 
     @Test
     void findById_ShouldReturnTeacherIfFound() {
-        when(teacherRepository.findById(1L)).thenReturn(Optional.of(teacher));
-        Teacher foundTeacher = teacherService.findById(1L);
-        assertEquals(teacher, foundTeacher);
+        teacherRepository.save(teacher);
+
+        Teacher foundTeacher = teacherService.findById(teacher.getId());
+
+        assertNotNull(foundTeacher);
+        assertEquals(teacher.getFirstName(), foundTeacher.getFirstName());
+        assertEquals(teacher.getLastName(), foundTeacher.getLastName());
     }
 
     @Test
     void findById_ShouldReturnNullIfNotFound() {
-        when(teacherRepository.findById(1L)).thenReturn(Optional.empty());
-        Teacher foundTeacher = teacherService.findById(1L);
+        Teacher foundTeacher = teacherService.findById(999L);
+
         assertNull(foundTeacher);
     }
 }
